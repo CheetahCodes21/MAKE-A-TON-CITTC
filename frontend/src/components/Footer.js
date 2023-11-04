78d2c0e7667272e02be901ee5520216648152fc1
Anudeep K K <anudeep2003@gmail.com>
2023-11-04 11:18:40 +0530

    "footer" position

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,60 +7,65 @@ import { FiPhoneCall } from "react-icons/fi";
 const Footer = () => {
   return (
     <>
-      <div className="container-fluid container-fluid-custom">
-        <div className="row">
-          <div className="col-md-4 col-sm-12 navigate">
-            <div
-              className="card card-footer p-5"
-              style={{ backgroundColor: "black", color: "white" }}
-            >
-              <div className="card-header card-header-custom">REACH US</div>
+      <div className="page-container">
+        <div className="container-fluid container-fluid-custom">
+          <div className="row"></div>
+          <div className="container-fluid container-fluid-custom" style={{ marginBottom: '0px' }}>
+            <div className="row">
+              <div className="col-md-4 col-sm-12 navigate">
+                <div
+                  className="card card-footer p-5"
+                  style={{ backgroundColor: "black", color: "white" }}
+                >
+                  <div className="card-header card-header-custom">REACH US</div>
 
-              <div className="card-body">
-                <p className="card-text">
-                  We Provide the best products at affordable prices
-                </p>
-                <div className="icons">
-                  <FaInstagram className="footer-icon" />
+                  <div className="card-body">
+                    <p className="card-text">
+                      We Provide the best products at affordable prices
+                    </p>
+                    <div className="icons">
+                      <FaInstagram className="footer-icon" />
 
-                  <FiYoutube className="footer-icon" />
+                      <FiYoutube className="footer-icon" />
 
-                  <FiMail className="footer-icon" />
+                      <FiMail className="footer-icon" />
 
-                  <FiPhoneCall className="footer-icon" />
+                      <FiPhoneCall className="footer-icon" />
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div className="col-md-4 col-sm-12 links">
+                <div className="card card-footer p-5">
+                  <div className="card-header card-header-custom">NAVIGATE</div>
+                  <ul className="list-group list-group-flush p-3">
+                    <li className="list-group-item">Home</li>
+                    <li className="list-group-item">Section -2</li>
+                    <li className="list-group-item">Section-3</li>
+                    <li className="list-group-item">Section-4</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-12 reach-us">
+                <div className="card card-footer p-5">
+                  <div className="card-header card-header-custom">LINKS</div>
+                  <ul className="list-group list-group-flush p-3">
+                    <li className="list-group-item">Privacy policy</li>
+                    <li className="list-group-item">
+                      Refund and cancellation policy
+                    </li>
+                    <li className="list-group-item">Terms and conditions</li>
+                  </ul>
+                </div>
+              </div>
+              <div
+                className="card-text copyright"
+                style={{ backgroundColor: "black", color: "white" }}
+              >
+                <center>&copy; 2023 Foodie</center>
+                <center>All rights reserved</center>
+              </div>
             </div>
-          </div>
-          <div className="col-md-4 col-sm-12 links">
-            <div className="card card-footer p-5">
-              <div className="card-header card-header-custom">NAVIGATE</div>
-              <ul className="list-group list-group-flush p-3">
-                <li className="list-group-item">Home</li>
-                <li className="list-group-item">Section -2</li>
-                <li className="list-group-item">Section-3</li>
-                <li className="list-group-item">Section-4</li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-12 reach-us">
-            <div className="card card-footer p-5">
-              <div className="card-header card-header-custom">LINKS</div>
-              <ul className="list-group list-group-flush p-3">
-                <li className="list-group-item">Privacy policy</li>
-                <li className="list-group-item">
-                  Refund and cancellation policy
-                </li>
-                <li className="list-group-item">Terms and conditions</li>
-              </ul>
-            </div>
-          </div>
-          <div
-            className="card-text copyright"
-            style={{ backgroundColor: "black", color: "white" }}
-          >
-            <center>&copy; 2023 Foodie</center>
-            <center>All rights reserved</center>
           </div>
         </div>
       </div>
