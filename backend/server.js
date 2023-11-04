48ed17816a7362030a3a32b3db0370a4ca08f4a6
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-11-04 21:10:08 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const route = require("./routes/route");
+const route = require("./Router/route");
 const cors = require("cors");
 
 const app = express();
