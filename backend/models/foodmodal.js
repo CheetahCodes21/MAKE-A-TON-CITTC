514e63cf268a79571d425e5130f489cd2eb6de7c
Anudeep K K <anudeep2003@gmail.com>
2023-11-04 21:22:24 +0530

    backend

--- backend/models/foodmodal.js
+++ backend/models/foodmodal.js
@@ -1,31 +1,39 @@
 const mongoose = require("mongoose");
 
+// Define a subdocument schema for the img field
+const imgSchema = new mongoose.Schema({
+  data: {
+    type: Buffer,
+    required: true,
+  },
+  contentType: {
+    type: String,
+    required: true,
+  },
+});
+
 const foodSchema = new mongoose.Schema({
-    Name: {
-        type: String,
-        required: true,
-    },
-    img: {
-        data: Buffer, // Store image data as Buffer
-        contentType: String, // Store the content type of the image
-        required: true,
-    },
-    desc: {
-        type: String,
-        required: true,
-    },
-    sales: {
-        type: String,
-        required: true,
-    },
-    likes: {
-        type: String,
-        required: true,
-    },
-    Ratings: {
-        type: String,
-        required: true,
-    },
+  Name: {
+    type: String,
+    required: true,
+  },
+  img: imgSchema, // Use the imgSchema for the img field
+  desc: {
+    type: String,
+    required: true,
+  },
+  sales: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: String,
+    required: true,
+  },
+  Ratings: {
+    type: String,
+    required: true,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model("FoodItem", foodSchema);
