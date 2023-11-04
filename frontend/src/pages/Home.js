7d1d99d59a25eaf3c701cf364be1e3b601f546f1
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-11-04 14:09:21 +0530

    carousel

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Carousel from '../components/Carousel'
 import Footer from '../components/Footer'
+import Card from '../components/card'
+
+
 
 
 const Home = () => {
-  return (
+ return(
+  <div>
+    <Navbar/>
     <div>
-      <Navbar/>
-      <Footer />
+    <Carousel/>
     </div>
-  )
+    <div>
+      <h1>Top Reciepes of the Week</h1>
+      <Card/>
+    </div>
+    
+    {/* <Footer/> */}
+  </div>
+ )
 }
 
 export default Home
